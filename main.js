ef5c4cdc6c18a506f4c21cce3ac38beff942176f
Cory Efird <cefird101@gmail.com>
2020-12-03 17:41:32 -0700

    Stuff.

--- main.js
+++ main.js
@@ -38,10 +38,11 @@ function main() {
     }
 
 //    let shader = transformShader(gl);
+
     let cubeMesh = new Mesh(gl, "models/cube.obj");
-    let sphereMesh = new Mesh(gl,"models/sphere8.obj", mat4.fromScaling(mat4.create(),[state.ground,state.ground,state.ground]));
+    let sphereMesh = new Mesh(gl,"models/sphere6.obj", mat4.fromScaling(mat4.create(),[state.ground,state.ground,state.ground]));
     let cylinderMesh = new Mesh(gl, "models/Cylinder.obj", mat4.fromScaling(mat4.create(),[10.0,600.0,10.0]));
-    let outerSphere = new Mesh(gl,"models/sphere8.obj",
+    let outerSphere = new Mesh(gl,"models/sphere6.obj",
         mat4.fromScaling(mat4.create(),[state.ceiling,state.ceiling,state.ceiling]), true);
     let ground = new GameObject(new Transform(), sphereMesh, assets.materials.white, shader);
     let ceiling = new GameObject(new Transform(), outerSphere, assets.materials.white, shader);
