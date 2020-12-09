c92ebba23b6c2a5a52c7d23acf83775dbc1a6188
Cory Efird <cefird101@gmail.com>
2020-12-09 15:49:25 -0700

    Uncapitalize Cylinder because it ruins everything.

--- main.js
+++ main.js
@@ -31,7 +31,7 @@ function main() {
 
     let cubeMesh = new Mesh(gl, "models/cube.obj");
     let sphereMesh = new Mesh(gl,"models/sphere6S.obj", mat4.fromScaling(mat4.create(),[state.ground,state.ground,state.ground]));
-    let cylinderMesh = new Mesh(gl, "models/Cylinder.obj", mat4.fromScaling(mat4.create(),[10.0,600.0,10.0]));
+    let cylinderMesh = new Mesh(gl, "models/cylinder.obj", mat4.fromScaling(mat4.create(),[10.0,600.0,10.0]));
     let outerSphere = new Mesh(gl,"models/sphere6.obj",
         mat4.fromScaling(mat4.create(),[state.ceiling,state.ceiling,state.ceiling]), true);
     let ground = new GameObject(new Transform(), sphereMesh, assets.materials.ground, shader);
